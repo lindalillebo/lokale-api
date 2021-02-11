9626d1c3e4667262df42155c0eb737fd7f4adfde
Linda Lillebø <linda_lillebo@hotmail.com>
2021-02-11 15:31:26 +0100

    Init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd42559891d04fcbe87d8774fc1e9c525'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a6575e69-e1f6-4ebf-bbec-3df345420875'
+};
